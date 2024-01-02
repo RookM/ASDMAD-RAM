d70ecffdff4d5ab3ab4fc739254685432845d371
Lycoris9 <arav_kyurem@hotmail.com>
2024-01-02 12:56:37 -0500

    search basis

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
+import android.widget.SearchView
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -16,7 +19,18 @@ import org.json.JSONObject
 import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
+    // on below line we are
+    // creating variables for listview
+    lateinit var programmingLanguagesLV: ListView
 
+    // creating array adapter for listview
+    lateinit var listAdapter: ArrayAdapter<String>
+
+    // creating array list for listview
+    lateinit var programmingLanguagesList: ArrayList<String>;
+
+    // creating variable for searchview
+    lateinit var searchView: SearchView
 private lateinit var binding: ActivityMainBinding
 
     fun edit (view: View?) {
@@ -72,6 +86,61 @@ private lateinit var binding: ActivityMainBinding
             writeToFile(jsonObject);
             signedInVal = true
         }
+
+        // initializing variables of list view with their ids.
+        programmingLanguagesLV = findViewById(R.id.idLVProgrammingLanguages)
+        searchView = findViewById(R.id.idSV)
+
+        // initializing list and adding data to list
+        programmingLanguagesList = ArrayList()
+        programmingLanguagesList.add("C")
+        programmingLanguagesList.add("C#")
+        programmingLanguagesList.add("Java")
+        programmingLanguagesList.add("Javascript")
+        programmingLanguagesList.add("Python")
+        programmingLanguagesList.add("Dart")
+        programmingLanguagesList.add("Kotlin")
+        programmingLanguagesList.add("Typescript")
+
+        // initializing list adapter and setting layout
+        // for each list view item and adding array list to it.
+        listAdapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            programmingLanguagesList
+        )
+
+        // on below line setting list
+        // adapter to our list view.
+        programmingLanguagesLV.adapter = listAdapter
+
+        // on below line we are adding on query
+        // listener for our search view.
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // on below line we are checking
+                // if query exist or not.
+                if (programmingLanguagesList.contains(query)) {
+                    // if query exist within list we
+                    // are filtering our list adapter.
+                    listAdapter.filter.filter(query)
+                } else {
+                    // if query is not present we are displaying
+                    // a toast message as no  data found..
+                    Toast.makeText(this@MainActivity, "No Language found..", Toast.LENGTH_LONG)
+                        .show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // if query text is change in that case we
+                // are filtering our adapter with
+                // new text on below line.
+                listAdapter.filter.filter(newText)
+                return false
+            }
+        })
     }
 
     fun getUsernameVal(): String {
